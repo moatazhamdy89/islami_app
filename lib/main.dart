23350d27efb2cbd4d512f5c0ffecc9314308a582
Moataz Hamdy <moatazhamdy162@gmail.com>
2026-09-14 02:55:56 +0300

    Initialize Project. Add Empty Home Screen.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/ui/home/home_screen.dart';
+import 'package:islami_app/utils/app_routes.dart';
+
+void main() {
+  runApp(IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.homeScreenRoute ,
+      routes:
+      {
+        AppRoutes.homeScreenRoute : (context) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,3 @@
+class AppRoutes {
+  static String homeScreenRoute = 'Home_Screen';
+}

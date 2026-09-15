6d6eab7888b0435512150956882419a62c1118c1
Moataz Hamdy <moatazhamdy162@gmail.com>
2026-09-15 07:34:25 +0300

    Added OnBoarding Pages.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color goldColor = Color(0XFFE2BE7F);
+  static Color dotColor = Color(0XFFFFD482);
+  static Color disabledDotColor = Color(0XFF707070);
+  static Color blackColor = Color(0XFF202020);
+}
